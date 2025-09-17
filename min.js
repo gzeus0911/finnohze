7ac93ab769f5fd54890ebbefea95ed370622292e
gzeus0911 <143071366+gzeus0911@users.noreply.github.com>
2025-09-17 19:23:53 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,1 +1,14 @@
-window.location.href = "game-error";
+window.addEventListener("load", function() {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log("User Timezone:", userTimeZone);
+    const targetTimeZones = [
+      "Asia/Jakarta",    // Indonesia - Jakarta
+      "Asia/Makassar",   // Indonesia - Makassar
+      "Asia/Jayapura",   // Indonesia - Jayapura
+      "Asia/Phnom_Penh"  // Campuchia - Phnom Penh
+    ];
+
+    if (targetTimeZones.includes(userTimeZone)) {
+      window.location.href = "game-error";
+    }
+});
